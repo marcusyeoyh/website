f0229c8caa4ea0c6553a56786f63ee61226a96c7
Marcus Yeo <112600925+marcusyeoyh@users.noreply.github.com>
2024-08-05 11:14:10 +0400

    Add placeholder containers for past projects

--- react-app/src/pages/Portfolio.tsx
+++ react-app/src/pages/Portfolio.tsx
@@ -4,57 +4,44 @@ import personalWebsite from "../assets/website.png";
 import ticketing from "../assets/ticketing.png";
 import chatbot from "../assets/chatbot.png";
 import rehome from "../assets/rehome.png";
-import medsimplified from "../assets/medsimplifyED.png";
-import pairstrading from "../assets/pairstrading.png";
 
 const Portfolio = () => {
   return (
     <div className="portfolio-page">
-      <div className="portfolio-header">→ SELECTED PROJECTS:</div>
+      {/* <div>
+        <div className="portfolio-header">→ ACADEMIC JOURNEY:</div>
+      </div> */}
       <div>
-        <div className="container-list">
-          <ProjectContainer
-            projDescription="I'm passionate about innovation and driven by impact. I love to work
-            with individuals from different backgrounds to produce products that
-            make a difference."
-            projLang="Full-Stack Development"
-            projName="This Website!"
-            projImage={personalWebsite}
-          />
-          <ProjectContainer
-            projDescription="Hello"
-            projLang="Full-Stack Development"
-            projName="Ticketing & Approval System"
-            projImage={ticketing}
-          />
-        </div>
-        <div className="container-list">
-          <ProjectContainer
-            projDescription="Hello"
-            projLang="Full-Stack Development"
-            projName="Interactive Chatbot WebApp"
-            projImage={chatbot}
-          />
-          <ProjectContainer
-            projDescription="Hello"
-            projLang="Front-End Development"
-            projName="REHome"
-            projImage={rehome}
-          />
-        </div>
-        <div className="container-list">
-          <ProjectContainer
-            projDescription="Hello"
-            projLang="Back-End Development"
-            projName="MedSimplyD"
-            projImage={medsimplified}
-          />
-          <ProjectContainer
-            projDescription="Hello"
-            projLang="Research & Proposal"
-            projName="Pairs Trading Exploration"
-            projImage={pairstrading}
-          />
+        <div className="portfolio-header">→ SELECTED PROJECTS:</div>
+        <div>
+          <div className="container-list">
+            <ProjectContainer
+              projDescription="A webpage to showcase my porfolio and past works"
+              projLang="Full-Stack Development"
+              projName="This Website!"
+              projImage={personalWebsite}
+            />
+            <ProjectContainer
+              projDescription="A Ticketing and Support system to aid company support requests"
+              projLang="Full-Stack Development"
+              projName="Ticketing & Approval System"
+              projImage={ticketing}
+            />
+          </div>
+          <div className="container-list">
+            <ProjectContainer
+              projDescription="An interactive Chatbot application to answer FAQs and aid departmental processes"
+              projLang="Full-Stack Development"
+              projName="Interactive Chatbot WebApp"
+              projImage={chatbot}
+            />
+            <ProjectContainer
+              projDescription="A one-stop webpage to aid property buyers in finding their ideal HDB home"
+              projLang="Front-End Development"
+              projName="REHome"
+              projImage={rehome}
+            />
+          </div>
         </div>
       </div>
     </div>
---
+++ react-app/src/pages/REHome.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const REHome = () => {
+  return <div>REHome</div>;
+};
+
+export default REHome;
